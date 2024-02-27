f3b1d4e6ef4bda63fed9cb5ff5a6bc984d5a84bb
mdiogc <mdiogcasko@gmail.com>
2024-02-27 16:27:19 +0000

    script servicios

---
+++ servicios-script.sh
@@ -0,0 +1,17 @@
+###########################################
+#
+# Autores: 
+# Inna <>
+# Marina Giacomaniello Castro <mdiogcasko@gmail.com> 
+#
+# Nombre: servicios-script.sh
+#
+# Objetivo: Debe recibir un argumento (nombre del servicio), si el usuario no lo indica, se le pedirá luego por teclado hasta que especifique un nombre.
+#
+
+# Entradas: 
+# Salidas: 
+#
+# Historial: 27/02/2024 #Primer esqueleto del ejercicio
+#
+###########################################
