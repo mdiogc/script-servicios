3c0ff723e98bc1c2690d4b66d7deeb6fd258a2da
OxideVanadium <96723185+OxideVanadium@users.noreply.github.com>
2024-02-28 08:44:21 +0000

    Update servicios-script.sh

--- servicios-script.sh
+++ servicios-script.sh
@@ -1,7 +1,7 @@
 ###########################################
 #
 # Autores: 
-# Inna <>
+# Inna Vdovitsyna <vanadiumoxide39@gmail.com>
 # Marina Giacomaniello Castro <mdiogcasko@gmail.com> 
 #
 # Nombre: servicios-script.sh
@@ -15,3 +15,31 @@
 # Historial: 27/02/2024 #Primer esqueleto del ejercicio
 #
 ###########################################
+
+srv=$1
+
+while [ -z "$srv" ]
+do
+    read -p "Indique el nombre del servicio: " srv
+done
+
+status=$( systemctl status $srv )
+active=$( systemctl is-active $srv )
+enabled_masked=$( systemctl is-enabled $srv )
+
+if [ "$status" =~ 'found.$' ]; then
+    echo "$srv no es un servicio"
+    exit 10
+fi
+
+if [ "$enabled_masked" = "masked" ]; then
+    masked="esta enmascarado"
+    enabled="esta deshabilitado"
+else 
+    masked="no esta enmascarado"
+    enabled=$( systemctl is-enabled $srv )
+fi
+
+echo "$srv $active"
+echo "$srv $enabled y $masked"
+echo "$status"
