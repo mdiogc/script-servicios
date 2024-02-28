f9cb5cecc94a08a5dd1bcab6874abd886e294c9d
OxideVanadium <96723185+OxideVanadium@users.noreply.github.com>
2024-02-28 10:11:39 +0000

    Update servicios-script.sh

--- servicios-script.sh
+++ servicios-script.sh
@@ -16,6 +16,7 @@
 #
 ###########################################
 
+#Obtenemos el nombre del processo
 srv=$1
 
 while [ -z "$srv" ]
@@ -23,16 +24,19 @@ do
     read -p "Indique el nombre del servicio: " srv
 done
 
+#Comprobamos si el processo indicado es un servicio
 status=$( systemctl status $srv )
-active=$( systemctl is-active $srv )
-enabled_masked=$( systemctl is-enabled $srv )
 
 if [ "$status" =~ 'found.$' ]; then
     echo "$srv no es un servicio"
     exit 10
 fi
 
-if [ "$enabled_masked" = "masked" ]; then
+#Comprobamos estado del servicio
+active=$( systemctl is-active $srv )
+enabled_masked=$( systemctl is-enabled $srv )
+
+if [ "$enabled_masked" == "masked" ]; then
     masked="esta enmascarado"
     enabled="esta deshabilitado"
 else 
@@ -40,6 +44,89 @@ else
     enabled=$( systemctl is-enabled $srv )
 fi
 
+#Mostramos estado del servicio
 echo "$srv $active"
 echo "$srv $enabled y $masked"
 echo "$status"
+
+#Configuramos opciones del menu que dependen del estado del servicio
+if [ "$active" == "inactive" ] && [ "$masked" == "no esta enmascarado" ]; then
+    to_active="Activar"
+else 
+    to_active="Desactivar"
+fi
+
+if [ "$enabled" == "enabled" ]; then
+    to_enable="Dishabilitar"
+else
+    to_enable="Habilitar"
+fi
+
+if [ "$masked" == "esta enmascarado" ]; then
+    to_mask="Desenmascarar"
+else
+    to_mask="Enmascarar"
+fi
+
+#Mostramos opciones del menu
+select option in "$to_active" "$to_enable" "$to_mask" "Configuracion" "Reiniciar(servicio activo)" "Reiniciar servicio(ultimo estado)" "Aplicar cambios(servicio activo)" "Aplicar cambios(ultimo estado del servicio)" "Configuracion del desarrallador" "Tiempo de carga del sistema" "Tiempo de carga del servicio" "Nivel de ejecucion" "Apagar equipo" "Reiniciar equipo" "Salir"
+do
+    case $option in
+    "$to_active" | 1 ) 
+        if [ "$to_active" == "Activar" ]; then
+            sudo systemctl start $srv
+        else
+            sudo systemctl stop $srv
+        fi
+    ;;
+    "$to_enable" | 2 )
+        if [ "$to_enable" == "Dishabilitar" ]; then
+            sudo systemctl disable $srv
+        else
+            sudo systemctl enable $srv
+        fi
+    ;;
+    "$to_mask" | 3 )
+        if [ "$to_mask" == "Desenmascarar" ]; then
+            sudo systemctl unmask $srv
+        else
+            sudo systemctl mask $srv
+        fi 
+    ;;
+    "Configuracion" | 4 )
+        systemctl show $srv
+    ;;
+    "Reiniciar(servicio activo)" | 5 )
+        sudo systemctl restart $srv
+    ;;
+    "Reiniciar servicio(ultimo estado)" | 6 )
+        sudo systemctl try-restart $srv
+    ;;
+    "Aplicar cambios(servicio activo)" | 7 )
+        sudo systemctl reload $srv
+    ;;
+    "Aplicar cambios(ultimo estado del servicio)" | 8 )
+        sudo systemctl reload-or-restart $srv
+    ;;
+    "Configuracion del desarrallador" | 9 )
+        sudo systemctl preset $srv
+    ;;
+    "Tiempo de carga del sistema" | 10 )
+        systemd-analyze
+    ;;
+    "Tiempo de carga del servicio" | 11 )
+        systemd-analyze blame | grep "$srv"
+    ;;
+    "Nivel de ejecucion" | 12 )
+        runlevel
+    ;;
+    "Apagar equipo" | 13 )
+        sudo systemctl isolate runlevel0.target
+    ;;
+    "Reiniciar equipo" | 14 )
+        sudo systemctl isolate runlevel6.target
+    ;;
+    "Salir" | 15 )
+        exit
+    esac 
+done
